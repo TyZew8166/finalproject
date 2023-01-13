d3171e4fc56f4f0953205c895438ee241a4010cd
Your Name <you@example.com>
2023-01-13 19:06:14 +0800

    新增跳轉頁面

--- app/src/main/java/tw/edu/pu/s1091802/googlemap/MapsActivity.java
+++ app/src/main/java/tw/edu/pu/s1091802/googlemap/MapsActivity.java
@@ -2,7 +2,10 @@ package tw.edu.pu.s1091802.googlemap;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +21,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,17 +34,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button nextPageBtn = (Button)findViewById(R.id.button);  //跳轉頁面
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MapsActivity.this , page2.class);  //從主畫面跳到搜尋畫面
+                startActivity(intent);
+            }
+        });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
---
+++ app/src/main/java/tw/edu/pu/s1091802/googlemap/page2.java
@@ -0,0 +1,14 @@
+package tw.edu.pu.s1091802.googlemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+}
